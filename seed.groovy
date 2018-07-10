ab857adb5eb9c55ceb16a73d39f711bd15c4c6bc
Vikram Modugu <moduguvikram@gmail.com>
2018-07-10 11:37:35 -0500

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,3 +1,46 @@
-job(jobName) {
-    description("Vikram's seed job with seed.groovy")
+def createDeploymentJob(jobName, repoUrl) {
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                        }
+                        branches('master')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
 }
+
+def createTestJob(jobName, repoUrl) {
+    multibranchPipelineJob(jobName) {
+        branchSources {
+            git {
+                remote(repoUrl)
+                includes('*')
+            }
+        }
+        triggers {
+            cron("H/3 * * * *")
+        }
+    }
+}
+
+def buildPipelineJobs() {
+    def repo = "https://github.com/moduguvikram"
+    def repoUrl = repo + jobName + ".git"
+    def deployName = jobName + "_deploy"
+    def testName = jobName + "_test"
+
+    createDeploymentJob(deployName, repoUrl)
+    createTestJob(testName, repoUrl)
+}
+
+buildPipelineJobs()
