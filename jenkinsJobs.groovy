2b8d2720e6e1eb91bc0a7e844117cc6320817126
Vikram Modugu <moduguvikram@gmail.com>
2018-07-10 12:06:08 -0500

    Update jenkinsJobs.groovy

--- jenkinsJobs.groovy
+++ jenkinsJobs.groovy
@@ -1,1 +1,23 @@
---test script
+def call(){
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+
+        // Execute different stages depending on the job
+        if(env.JOB_NAME.contains("SpringRestServices")){
+            packageArtifact()
+        } else if(env.JOB_NAME.contains("test")) {
+            //buildAndTest()
+        }
+    }
+}
+
+def packageArtifact(){
+    stage("Package artifact") {
+        sh "mvn clean install"
+    }
+}
+
+
+}
